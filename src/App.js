a21aea1deda019230c718ab8635832fb4d421460
Tehseen Afzal <abuashja@gmail.com>
2021-06-17 23:59:06 +0500

    Update App.js
    Add Grid Layout to application

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import React from "react";
 import "./App.css";
 import NavBar from "./Components/NavBar";
+import { makeStyles } from "@material-ui/core/styles";
+import { Paper, Grid } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
     <div>
-      <NavBar />
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <NavBar />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper className={classes.paper}>xs=12 sm=6</Paper>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Paper className={classes.paper}>xs=12 sm=6</Paper>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Paper className={classes.paper}>xs=6 sm=3</Paper>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Paper className={classes.paper}>xs=6 sm=3</Paper>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Paper className={classes.paper}>xs=6 sm=3</Paper>
+          </Grid>
+          <Grid item xs={6} sm={3}>
+            <Paper className={classes.paper}>xs=6 sm=3</Paper>
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
