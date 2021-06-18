2dde17da893cc19287137bff5ea0f1c67bb29c14
Tehseen Afzal <abuashja@gmail.com>
2021-06-18 22:48:40 +0500

    Update App.js
    Update layout

--- src/App.js
+++ src/App.js
@@ -20,26 +20,15 @@ function App() {
   return (
     <div>
       <div className={classes.root}>
-        <Grid container spacing={3}>
+        <Grid container spacing={2}>
           <Grid item xs={12}>
             <NavBar />
           </Grid>
-          <Grid item xs={12} sm={6}>
+          <Grid item xs={12}>
             <Paper className={classes.paper}>xs=12 sm=6</Paper>
           </Grid>
-          <Grid item xs={12} sm={6}>
-            <Paper className={classes.paper}>xs=12 sm=6</Paper>
-          </Grid>
-          <Grid item xs={6} sm={3}>
-            <Paper className={classes.paper}>xs=6 sm=3</Paper>
-          </Grid>
-          <Grid item xs={6} sm={3}>
-            <Paper className={classes.paper}>xs=6 sm=3</Paper>
-          </Grid>
-          <Grid item xs={6} sm={3}>
-            <Paper className={classes.paper}>xs=6 sm=3</Paper>
-          </Grid>
-          <Grid item xs={6} sm={3}>
+
+          <Grid item xs={12}>
             <Paper className={classes.paper}>xs=6 sm=3</Paper>
           </Grid>
         </Grid>
